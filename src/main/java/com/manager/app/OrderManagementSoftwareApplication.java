262ea545c2c325d7fe72242209921b034e312a94
DaviDemarqui <davidemarqui@genesyssoft.com.br>
2022-04-28 14:18:31 -0300

    APP-0 - First Commit

---
+++ src/main/java/com/manager/app/OrderManagementSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.manager.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementSoftwareApplication.class, args);
+	}
+
+}
